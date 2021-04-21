f87b5b693628bde72940345c896ab6e5b051f4a0
Reyhan Alkadri <reyhanalkadri@gmail.com>
2021-04-21 12:02:06 +0700

    testing

---
+++ lat.js
@@ -0,0 +1,14 @@
+var value1 = 15
+var value2 = 5
+
+var calTambah = value1 + value2;
+console.log(calTambah);
+
+var calKurang = value1 - value2;
+console.log(calKurang);
+
+var calKali = value1 * value2;
+console.log(calKali);
+
+var calBagi = value1 / value2;
+console.log(calBagi);
